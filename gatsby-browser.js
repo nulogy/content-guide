761e65bd5ff23d85c868cb3f1d6a4cd15c1999aa
Matt Dunn <mattd@nulogy.com>
2021-08-05 12:55:59 -0400

    converts index page to mdx

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,6 @@ import { NDSProvider } from "@nulogy/components";
 import Layout from "./src/components/Layout";
 
 const newTheme = {
-  fontFamilies: {
-    base: "serif",
-  },
   fontSizes: {
     small: "16px",
     medium: "20px",
@@ -13,15 +10,10 @@ const newTheme = {
 };
 
 export const wrapPageElement = ({ element, props }) => {
-  let path = props.location?.pathname;
   
   return (
     <NDSProvider theme={newTheme}>
-      {path === "/" ? (
-        <div>{element}</div>
-      ) : (
-        <Layout {...props}>{element}</Layout>
-      )}
+      <Layout {...props}>{element}</Layout>
     </NDSProvider>
   );
 };
