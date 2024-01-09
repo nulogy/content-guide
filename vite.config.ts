675d2b8812e452e061d630f7590e24db53f112f3
Haider Alshamma <haidera@nulogy.com>
2024-01-09 16:21:38 -0500

    Use / as a baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/content-guide/",
-	plugins: [
-		{
-			enforce: "pre",
-			...mdx({
-				providerImportSource: "@mdx-js/react",
-			}),
-		},
-		react(),
-	],
+  plugins: [
+    {
+      enforce: "pre",
+      ...mdx({
+        providerImportSource: "@mdx-js/react",
+      }),
+    },
+    react(),
+  ],
 });
